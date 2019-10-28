6c37a6c1da9a35a59e776dcefa44ce83b7713bae
Russ Paul-Jones <russ@paul-jones.org>
2019-10-27 17:29:59 -0700

    Sort of painting.
    Actually, not painting the mandelbrot at all. but it looks like we're going through the (not really Mandelbrot) calculations. Weird delays.

--- MandelbrotSet.py
+++ MandelbrotSet.py
@@ -4,6 +4,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 from kivy.graphics import Color, Ellipse, Rectangle
+from kivy.clock import Clock
 from mapper import ViewPortMapper
 
 class Arena(Widget):
@@ -27,15 +28,26 @@ class Arena(Widget):
 			Color(0.7, 0.7, 0.1)
 			Ellipse(pos=self.pos, size=self.size)
 
+def ColorMapper(z):
+	dist = abs(z)*100
+	if dist > 255:
+		dist = 255
+	return int(dist)
+
+def MixColors(color1, color2, ratio):
+	red = color1.r*(1-ratio) + color2.r*ratio
+	green = color1.g*(1-ratio) + color2.g*ratio
+	blue = color1.b*(1-ratio) + color2.b*ratio
+	return Color(red, green, blue)
 
 class MandApp(App):
 	def build(self):
 		self.root = layout = FloatLayout()
 		layout.bind(size=self._update_rect, pos=self._update_rect)
 
-		ll = complex(-2.0, -2.0)
-		ur = complex(2.0, 2.0)
-		caption = 'Mandelbrot Set on grid from {0} to {1}'.format(ll, ur)
+		self.ll = complex(-2.0, -2.0)
+		self.ur = complex(2.0, 2.0)
+		self.caption = 'Mandelbrot Set on grid from {0} to {1}'.format(self.ll, self.ur)
 
 		with layout.canvas.before:
 			Color(0.1, .9, 0.1, 1)  # green; colors range from 0-1 not 0-255
@@ -60,6 +72,32 @@ class MandApp(App):
 			ellipsePos = [instanceX - (instanceHeight-instanceWidth)/2, instanceY]
 		self.ellipse.pos = ellipsePos
 		self.ellipse.size = ellipseSize
+		Clock.schedule_once(self.CalculateMandelbrotDisplay, 1)
+
+	def CalculateMandelbrotDisplay(self, dt):
+		colorMin = Color(1, 0, 0)
+		colorMax = Color(0, 1, 0)
+		windowWidth = int(self.rect.size[0])
+		windowHeight = int(self.rect.size[1])
+		mapper = ViewPortMapper(self.ll, self.ur, windowWidth, windowHeight)
+		self.display = []
+		for x in range(windowWidth):
+			self.display.append([])
+			for y in range(windowHeight):
+				z = mapper.Map(x, y)
+				self.display[x].append(MixColors(colorMin, colorMax, ColorMapper(z)/256))
+		self.PaintMandelbrot()
+	
+	def PaintMandelbrot(self):
+		canvas = self.root.canvas
+		canvas.clear()
+		with canvas:
+			for x in range(self.root.size[0]):
+				Color(0,0,0)
+				Rectangle(pos=[x,0], size=[1, self.root.size[1]])
+				for y in range(self.root.size[1]):
+					Color(self.display[x][y])
+					Rectangle(pos=[x,y], size=[1,1])
 
 if __name__ == '__main__':
 	MandApp().run()
